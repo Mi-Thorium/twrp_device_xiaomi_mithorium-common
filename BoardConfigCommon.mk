878418a3a443d82086c84eb1eb887d51f58627cc
imasaru <evanmr4@gmail.com>
2018-09-09 23:19:58 -0500

    mithorium-common: Blacklist input "hbtp_vm" to hide cursor
    Blacklist device that is identified as mouse to hide cursor.
    
    Change-Id: I399c20b62bf8fa11fa53af75b0c11db8cd419fd7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 # TWRP
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS ?= 255
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
