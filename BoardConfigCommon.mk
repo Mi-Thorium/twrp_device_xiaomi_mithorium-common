2d73af5effd2cd0404429e863c0e3f830303c54d
Roger Ellis <ellisr@google.com>
2020-05-13 16:56:10 -0700

    mithorium-common: Allow ELF prebuilts in PRODUCT_COPY_FILES.
     * ELF prebuilts were banned in PRODUCT_COPY_FILES starting
       May 11th. See b/156534160 for long-term fix which should
       override this change.
    
    Change-Id: Icd25b40ca4e4285319775046a1b70de30572be07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,6 +22,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Build
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true loop.max_part=7
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0x78B0000
