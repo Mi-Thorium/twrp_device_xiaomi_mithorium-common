0ec5924ab15eb21dd2fec71a92ad7b68cc195f75
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 00:07:02 +0800

    mithorium-common: Set TARGET_NO_BOOTLOADER

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,3 +18,6 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
