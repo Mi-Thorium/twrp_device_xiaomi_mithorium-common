c737a3db58b56413cb83ded39abc4e78930a50d2
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 00:13:01 +0800

    mithorium-common: Move console kernel parameters to separate line

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,5 +23,5 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x7
-8B0000 androidboot.usbconfigfs=true loop.max_part=7
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci androidboot.usbconfigfs=true loop.max_part=7
+BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0x78B0000
