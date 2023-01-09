0ba7d7d8455c113a23bed504e9e35949d264f49c
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-09 22:20:47 +0800

    mithorium-common: Build libion library
    * Required for kernel 4.19 blobs
    
    Change-Id: I1ca27b4d3f878f8310dc50b61bb2acf858c5da0a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,11 +78,13 @@ PLATFORM_SECURITY_PATCH := 2099-12-31
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
     libxml2 \
     vendor.display.config@1.0 \
     vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
